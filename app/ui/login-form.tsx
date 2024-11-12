38ce6426638089d086a8dbdb2c12c904c1027905
Wallewdev <wallewdev@gmail.com>
2024-11-12 15:57:24 +0100

    Fixing Eslint Error

--- app/ui/login-form.tsx
+++ app/ui/login-form.tsx
@@ -2,10 +2,11 @@ import { lusitana } from '@/app/ui/fonts';
 import {
   AtSymbolIcon,
   KeyIcon,
-  ExclamationCircleIcon,
 } from '@heroicons/react/24/outline';
 import { ArrowRightIcon } from '@heroicons/react/20/solid';
 import { Button } from './button';
+
+// ExclamationCircleIcon,
 
 export default function LoginForm() {
   return (
